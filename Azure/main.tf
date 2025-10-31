dd4a3f273dbe1e438090349e1d1e62e85c91ea2a
Saied Makshat <saied.makshat@gmail.com>
2025-10-31 20:11:24 +0200

    add teraform

--- Azure/main.tf
+++ Azure/main.tf
@@ -5,4 +5,11 @@ terraform {
       version = "=4.5.0"
     }
   }
+
+    backend "azurerm" {
+    resource_group_name = "your-resource-group"  # Replace with your resource group name
+    storage_account_name = "your-storage-account"  # Replace with your storage account name
+    container_name = "terraform-state"  # Replace with your desired container name
+    key = "terraform.tfstate"  # Optional: Specify the filename within the container (defaults to 'terraform.tfstate')
+  }
 }
