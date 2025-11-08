4e4d12989e1e1bcccad83c5ad06cc65d8e418e1e
Saied Makshat <saied.makshat@gmail.com>
2025-11-08 08:15:48 +0200

    aks hub and spoke design

---
+++ Azure-AKS/providers.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+
+#   backend "azurerm" {
+#     resource_group_name  = "Terraform"
+#     storage_account_name = "terraformsaied"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  features {}
+}
+provider "random" {
+  # Configuration options
+}
---
+++ Azure-AKS/variables.tf
@@ -0,0 +1,9 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "location" {
+  default = "eastus"
+  type    = string
+}
